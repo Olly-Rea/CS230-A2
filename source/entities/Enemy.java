1b4a359c1dde216a09da47fa44fdbc04494eb6bf
JamesH0gg <44364237+JamesH0gg@users.noreply.github.com>
2019-11-19 20:26:34 +0000

    Updated with javadoc

---
+++ source/entities/Enemy.java
@@ -0,0 +1,22 @@
+package entities;
+/**
+* @author James Hogg
+* @version 1.0
+* An Enemy is a player-hostile entity that moves according to an algorithm
+* based on the environment
+*/
+/**
+* Provides the next move based on the current map state
+* @param Map The current map state
+* @return Returns a vector containing the next position
+*/
+public abstract Vector pathingAlgorithm(Map);
+/**
+* Checks to see if the enemy has collided with the player
+*/
+protected PlayerCheck(Map);
+/**
+* Constructs an enemy
+*/
+public Enemy() {
+}
