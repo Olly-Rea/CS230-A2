edc24411b5cefd982584ed2086e64434537127ae
treik2 <48773729+treik2@users.noreply.github.com>
2019-11-20 16:51:00 +0000

    added nextLine method to FileHandler

--- source/utils/FileHandler.java
+++ source/utils/FileHandler.java
@@ -1,24 +1,30 @@
 package utils;
-import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.BufferReader;
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
 
 public class FileHandler {
-  private BufferReader reader;
+	private BufferedReader reader;
 
-  public FileHandler(String path) {
+	public FileHandler(String path) {
 
-    try {
-      FileReader fReader = new FileReader(path);
-      reader = new BufferReader(fReader);
-    }
-    catch (FileNotFoundException e) {
-      System.out.println ("Cannot open " + filename);
-      System.exit (0);
-    }
+		try {
+			FileReader fReader = new FileReader(path);
+			reader = new BufferedReader(fReader);
+		} catch (FileNotFoundException e) {
+			System.out.println("Cannot open " + path);
+			System.exit(0);
+		}
+	}
 
-  }
+	public String nextLine() {
+		try {
+			return reader.readLine();
+		} catch (IOException e) {
+			return "";
+		}
+	}
 
 }
