9b751281b96eb0ffa41e3ab112a3015a43ccc528
Scott <ScottE.barr@outlook.com>
2019-11-25 12:05:58 +0000

    Implemented SmartTargeter.java with comments

--- source/entities/enemies/SmartTargeter.java
+++ source/entities/enemies/SmartTargeter.java
@@ -1,261 +1,103 @@
-package game.entities;
+package entities.enemies;
 
-import javafx.scene.canvas.GraphicsContext;
-import utils.Direction;
-import utils.Node;
-import game.cells.Cell;
-import game.cells.Cell.CellType;
-import javafx.scene.paint.Color;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Stack;
 
-import game.Map;
+import cells.Cell;
+import cells.CellType;
+import controllers.MapController;
+import entities.Enemy;
+import entities.Player;
+import utils.Direction;
+import utils.Vector;
 
-public class SmartFollower extends Enemy {
+/**
+ * SmartTargeter class which always takes the path which leads to the Player.
+ * 
+ * @author Scott Barr
+ */
+public class SmartTargeter extends Enemy {
 
     private Direction dir;
     private Player player;
 
-    public SmartFollower(int xPos, int yPos, Player p) {
-        super(xPos, yPos);
-        player = p;
+    public SmartTargeter(Vector pos, Player player) {
+        super(pos);
+        this.player = player;
     }
 
-    public void render(GraphicsContext gc) {
-        gc.setFill(Color.RED);
-        gc.fillOval(xPos * Cell.size - 3 + Cell.size / 2, yPos * Cell.size - 3 + Cell.size / 2, 6, 6);
-        gc.strokeOval(xPos * Cell.size - 3 + Cell.size / 2, yPos * Cell.size - 3 + Cell.size / 2, 6, 6);
+    /**
+     * 
+     */
+    public void render() {
     }
 
-    public void algorithm(Map map) {
-        dir = null;
+    /**
+     * Creates a distance grid which flood fills from the players position until
+     * every possible connecting cell is filled.
+     * 
+     * @param map The map containing the cells
+     * @return A 2d Array of integers the same size as the map filled with distances
+     *         from the Players position.
+     */
+    private Integer[][] makeDistGrid(MapController map) {
+        // Initialise Variables
         LinkedList<Cell> queue = new LinkedList<>();
         Integer[][] distGrid = new Integer[map.height][map.width];
-        distGrid[player.yPos][player.xPos] = 0;
-        queue.add(map.getCell(player.xPos, player.yPos));
+        Vector playerPos = player.getPos();
 
+        // Set position at player to 0;
+        distGrid[playerPos.getY()][playerPos.getX()] = 0;
+        queue.add(map.getCell(playerPos.getX(), playerPos.getY()));
+
+        // If queue is not empty then there are still connecting cells left to be filled
         while (!queue.isEmpty()) {
-            Cell q = queue.poll();
-            for (Direction d : Direction.values()) {
-                Cell next = map.getCell(q.x, q.y, d);
-                if (next.getType() == CellType.GROUND) {
-                    int dist = distGrid[q.y][q.x] + 1;
-                    if (distGrid[next.y][next.x] == null) { 
+            Cell q = queue.poll(); // q = the next cell in the queue
+            for (Direction d : Direction.values()) { // For all directions (UP/RIGHT/DOWN/LEFT)
+                Cell next = map.getCell(new Vector(q.x, q.y), d); // get the next cell
+                if (next.getType() == CellType.GROUND) { // if the cell is of type GROUND
+                    int dist = distGrid[q.y][q.x] + 1; // distance is incremented by 1
+                    // if distGrid at next position is empty then add it to the queue & set to dist
+                    if (distGrid[next.y][next.x] == null) {
                         distGrid[next.y][next.x] = dist;
                         queue.add(next);
                     }
-                }                
+                }
             }
         }
 
+        return distGrid;
+    }
+
+    public void algorithm(MapController map) {
+        // Generate the distance grid and set minDist to null and dir to null
+        Integer[][] distGrid = makeDistGrid(map);
         Integer minDist = null;
-        for (Direction d : Direction.values()) {
-            Cell next = map.getCell(xPos, yPos, d);
-                if (next.getType() == CellType.GROUND) {
-                    Integer dist = distGrid[yPos+d.Y][xPos+d.X];
-                    if (minDist == null) {
+        dir = null;
+
+        // If the distGrid at the enemies position is 0 then do not move
+        if (distGrid[pos.getY()][pos.getX()] != 0) {
+            for (Direction d : Direction.values()) { // for all Directions (UP, RIGHT, DOWN, LEFT)
+                Cell next = map.getCell(new Vector(pos.getX(), pos.getY()), d); // get the next cell in that direction
+                if (next.getType() == CellType.GROUND) { // Confirm it's a ground cell
+                    Integer dist = distGrid[pos.getY() + d.Y][pos.getX() + d.X]; // check the distance at that cell in
+                                                                                 // distGrid
+                    // If minDist is null and dist isn't null then set minDist to be dist
+                    // and set facing direction of the enemy to be the direction d
+                    if (minDist == null && dist != null) {
                         minDist = dist;
                         dir = d;
-                    } else {
+                    } else { // otherwise if dist is smaller than minDist set the new direction as d
                         if (dist < minDist) {
                             minDist = dist;
                             dir = d;
                         }
                     }
-                }        
+                }
+            }
         }
 
-        if (!(yPos == player.yPos && xPos == player.xPos)) {
-            if (dir != null) {
-                xPos += dir.X;
-                yPos += dir.Y;
-            } 
+        if (dir != null) { // if dir is null then do not move.
+            pos.add(dir); // otherwise add the dir to the positon.
         }
-  
     }
-
-
-
 }
-
-    // private ArrayList<Cell> path;
-    // private void find(Map map) {
-    //     int minDist = Integer.MAX_VALUE;
-    //     for (Direction d : Direction.values()) {
-    //         path = new ArrayList<>();
-    //         path.add(map.getCell(xPos, yPos));
-    //         Cell nextCell = map.getCell(xPos+d.X, yPos+d.Y);
-    //         if (nextCell.getType() == CellType.GROUND) {
-    //             path.add(nextCell);
-    //             int dist = find(xPos + d.X, yPos + d.Y, map, 0);
-    //             System.out.println(d + " : " + dist);
-    //             if (dist < minDist) {
-    //                 minDist = dist;
-    //                 dir = d;
-    //             }
-    //         }
-    //     }
-    // }
-
-    // private int find(int x, int y, Map map, int dist) {
-    //     if (x == player.getXPos() && y == player.getYPos()) return dist;
-    //     for (Direction d : Direction.values()) {
-    //         Cell nextCell = map.getCell(x+d.X, y+d.Y);
-    //         if (nextCell.getType() == CellType.GROUND && !path.contains(nextCell)) {
-    //             gc.fillOval(nextCell.x * Cell.size - 3 + Cell.size / 2, nextCell.y * Cell.size - 3 + Cell.size / 2, 6, 6);
-    //             path.add(nextCell);
-    //             return find(x + d.X, y + d.Y, map, dist+1);
-    //         }
-    //     }
-    //     return Integer.MAX_VALUE;
-    // }
-
-    // public void algorithm(Map map) {
-    //     find(map);
-    //     System.out.println(dir);
-    // }
-
-    // public Integer searchDist(Map map, Cell c) {
-    //     Queue<Cell> queue = new LinkedList<>();
-    //     HashMap<Cell, Integer> dist = new HashMap<>();
-    //     int distance = 0;
-    //     queue.add(c);
-    //     dist.put(c, 0);
-    //     while (!queue.isEmpty()) {
-    //         Cell next = queue.poll();
-    //         distance++;
-    //         if (next.x == player.xPos && next.y == player.yPos)
-    //             return dist.get(next);
-    //         ArrayList<Cell> surrounding = map.surrounding(next, CellType.GROUND);
-    //         for (int i = 0; i < surrounding.size(); i++) {
-    //             Cell cell = surrounding.get(i);
-    //             if (!dist.containsKey(cell)) {
-    //                 queue.add(cell);
-    //                 dist.put(cell, distance);
-    //             }
-    //         }
-    //     }
-    //     return null;
-    // }
-
-    // public void algorithm(Map map) {
-    //     Integer min = null;
-    //     dir = null;
-    //     for (Direction d : Direction.values()) {
-    //         Cell cell = map.getCell(xPos+d.X, yPos+d.Y);
-    //         if (Cell.is(cell, CellType.GROUND)) {
-    //             Integer dist = searchDist(map, cell);
-    //             System.out.println(d + " : " + dist);
-    //             if (min == null && dist != null) {
-    //                 min = dist;
-    //                 dir = d;
-    //             }
-    //             else {
-    //                 if (dist < min) {
-    //                     min = dist;
-    //                     dir = d;
-    //                 }
-    //             }
-    //         }
-    //     }
-
-    //     if (dir != null) {
-    //         // xPos += dir.X;
-    //         // yPos += dir.Y;   
-    //     }
-    // }
-
-
-    
-
-    // private int dist(Cell cell) {
-    //     return (Math.abs(player.xPos - cell.x) + Math.abs(player.yPos - cell.y));
-    // }
-
-    // public void astar(Map map) {
-    //     HashMap<Cell, Integer> fMap = new HashMap<>();
-    //     HashMap<Cell, Integer> dMap = new HashMap<>();
-
-    //     ArrayList<Cell> open = new ArrayList<>();
-    //     ArrayList<Cell> closed = new ArrayList<>();
-
-    //     Cell start = map.getCell(xPos, yPos);
-    //     open.add(start);
-    //     dMap.put(start, 0);
-    //     fMap.put(start, dist(start));
-
-    //     while(!open.isEmpty()) {
-    //         int minF = Integer.MAX_VALUE;
-    //         int index = 0;
-    //         for (int i = 0; i < open.size(); i++) {
-    //             Cell t = open.get(i);
-    //             int f = fMap.get(t);
-    //             if (f < minF) {
-    //                 minF = f;
-    //                 index = i;
-    //             }
-    //         }
-
-    //         Cell q = open.remove(index);
-    //         ArrayList<Cell> successors = map.surrounding(q, CellType.GROUND);    
-    //         for (int i = 0; i < successors.size(); i++) {
-    //             Cell c = successors.get(i);
-    //             if (c.x == player.xPos && c.y == player.yPos) {
-    //                 System.out.println(dMap.get(c));
-    //                 return;
-    //             }
-
-    //             int g = 1 + dMap.get(q);
-    //             int h = dist(c);
-
-    //             if ((closed.contains(c) || open.contains(c)) && fMap.get(c) < g+h) {
-    //                 continue;
-    //             } else {
-    //                 dMap.put(c, 1 + dMap.get(q));
-    //                 fMap.put(c, dMap.get(c) + dist(c));
-    //                 open.add(c);
-    //             }
-    //         }
-    //         closed.add(q);
-    //     }
-        
-    //     System.out.println("Dist : " + dMap.get(map.getCell(player.xPos, player.yPos)));
-    // }
-
-    // public void algorithm(Map map) {
-    //     ArrayList<Node<Cell>> marked = new ArrayList<>();
-    //     Stack<Node<Cell>> stack = new Stack<>();
-    //     Cell startCell = map.getCell(xPos, yPos);
-    //     Node<Cell> root = new Node<>(startCell, 0);
-    //     stack.push(root);
-    //     marked.add(root);
-        
-
-    //     while (!stack.isEmpty()) {
-    //         Node<Cell> head = stack.peek();
-    //         ArrayList<Cell> successors = map.surrounding(head.obj, CellType.GROUND);
-    //         for (int i = 0; i < successors.size(); i++) {
-    //             Node<Cell> next = new Node<>(successors.get(i), head.dist+1);
-    //             if (next.obj.x == player.xPos && next.obj.y == player.yPos) {
-    //                 System.out.println("true");
-    //             }
-    //             gc.fillOval(next.obj.x * Cell.size - 3 + Cell.size / 2, next.obj.y * Cell.size - 3 + Cell.size / 2, 6, 6);
-    //             if (!stack.contains(next)) {
-    //                 if (!marked.contains(next)) {
-    //                     marked.add(next);
-    //                     stack.push(next);
-    //                 } else {
-    //                     stack.pop();
-    //                 }
-    //             }
-    //         }
-    //     }
-        
-    // }
