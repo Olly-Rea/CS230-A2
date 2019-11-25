9f8c4c99400d047e6bb1ac46052367f4210fe07b
Scott <ScottE.barr@outlook.com>
2019-11-25 11:26:48 +0000

    Added StraightLineEnemy algorithm implementation

--- source/entities/enemies/StraightLineEnemy.java
+++ source/entities/enemies/StraightLineEnemy.java
@@ -1,15 +1,24 @@
 package entities.enemies;
 
+import cells.Cell;
+import cells.CellType;
+import controllers.MapController;
+import entities.Enemy;
+import utils.Direction;
+import utils.Vector;
 
-public class StraightLineEnemy extends Enemy{
+public class StraightLineEnemy extends Enemy {
+
+	private Direction dir;
 
 	/**
 	 * Straight line.
 	 *
 	 * @param vector the position of th straight line enemy
 	 */
-	public StraightLine(Vector vector) {
-
+	public StraightLineEnemy(Vector pos, Direction dir) {
+		super(pos);
+		this.dir = dir;
 	}
 
 	/**
@@ -17,8 +26,13 @@ public class StraightLineEnemy extends Enemy{
 	 *
 	 * @param map the map
 	 */
-	private void algorithm(Map map) {
+	public void algorithm(MapController map) {
+		Cell next = map.getCell(pos, dir);
+		if (next.getType() == CellType.WALL) {
+			dir = dir.cw().cw();
+		}
 
+		this.pos.add(dir);
 	}
 
     /**
