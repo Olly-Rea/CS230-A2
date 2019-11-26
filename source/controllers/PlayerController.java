d4621947e027b49e2cd3d19e2c4cfc341e0716de
DannyClena <57802703+DannyClena@users.noreply.github.com>
2019-11-26 13:46:18 +0000

    Updated and cleaned player+controller
    Added potental export type and added imports

--- source/controllers/PlayerController.java
+++ source/controllers/PlayerController.java
@@ -1,8 +1,15 @@
 package controllers;
 
+import entities.Cell;
+import entities.CellType;
+import entities.Item;
+import entities.ItemType;
+import entities.Player;
+import utils.Vector;
+
 /**
- * PlayerController.java
- * Class to handle player movement and rendering
+ * PlayerController.java Class to handle player movement and rendering
+ *
  * @author Scott, Danny
  */
 public class PlayerController {
@@ -21,8 +28,9 @@ public class PlayerController {
 	}
 
 	/**
-   * Takes a direction and determines if the player can move into the desired
-   * cell based on their inventory
+	 * Takes a direction and determines if the player can move into the desired cell
+	 * based on their inventory
+	 *
 	 * @param dir the direction the player is attempting to move
 	 * @param mc  the map controller to find the Cell in the dir
 	 * @author Danny
@@ -31,22 +39,23 @@ public class PlayerController {
 		Vector current = getPlayerPos();
 		Vector desired;
 		if (dir == MOVES.UP) {
-			desired = new Vector(current.getX(),current.getY() + 1);
+			desired = new Vector(current.getX(), current.getY() + 1);
 		} else if (dir == MOVES.DOWN) {
-			desired = new Vector(current.getX(),current.getY() - 1);
-		} else if (dir==MOVES.LEFT) {
-			desired = new Vector(current.getX() + 1,current.getY());
-		} else if (dir==MOVES.RIGHT) {
-			desired = new Vector(current.getX() - 1,current.getY());
+			desired = new Vector(current.getX(), current.getY() - 1);
+		} else if (dir == MOVES.LEFT) {
+			desired = new Vector(current.getX() + 1, current.getY());
+		} else if (dir == MOVES.RIGHT) {
+			desired = new Vector(current.getX() - 1, current.getY());
 		}
 		Cell desiredCell = mc.getCell(desired.getX(), desired.getY());
 		if (validMove(desiredCell)) {
-			// player.UPDATE LOCATION?
+			player.setPos(desired);
 		}
 	}
 
 	/**
-   * PPrivate method to determine if a move is valid based on player inventory
+	 * Private method to determine if a move is valid based on player inventory
+	 *
 	 * @param cell the cell in the direction the player wants to move
 	 * @return a boolean value true if the move is valid, i.e not walikng into a
 	 *         wall, false if the move is invalid and not possible
@@ -70,16 +79,17 @@ public class PlayerController {
 			Item greenKey = new Item(ItemType.GREENKEY);
 			valid = player.useItem(greenKey);
 		} else if (move == (CellType.TOKEN)) {
-			//valid = player.useTokens(); // Need method to check tokens required
+			// valid = player.useTokens(); // Need method to check tokens required
 		} else if (move == (CellType.TELEPORTER)) {
-      valid = true;
-      //Cell.getLinkedPos() Update player location 
-    }
+			valid = true;
+			// player.setPos(Cell.getLinkedPos());
+		}
 		return valid;
 	}
 
 	/**
-   * Check if the cell requires a shoe type and if the player posseses it
+	 * Check if the cell requires a shoe type and if the player posseses it
+	 *
 	 * @param cell the cell being checked against the player, if a certain item is
 	 *             required for that cell type
 	 * @return a boolean value, if true, the player is on a block which kills the
@@ -117,10 +127,13 @@ public class PlayerController {
 	}
 
 	/**
-	 * @return an array of strings representing the players position and inventory
-	 * @author xxxxx
+	 * Method to get a int array {X,Y,fireShoes, flippers,tokens, red,
+	 * green,blue,yellow}
+	 *
+	 * @return an array of ints representing the players position and inventory
+	 * @author Danny
 	 */
-	public String[] export() {
-		return null;
+	public int[] export() {
+		return player.export();
 	}
 }
--- source/entities/Player.java
+++ source/entities/Player.java
@@ -1,162 +1,228 @@
 package entities;
 
+import utils.Vector;
+
 /**
-* Player.java
-* The player class is responsible for storing and manipulating the
-* player inventory and its attributes
-* @author Daniel Clenaghan
-*/
+ * Player.java The player class is responsible for storing and manipulating the
+ * player inventory and its attributes
+ *
+ * @author Daniel Clenaghan
+ */
 
 public class Player extends Entity {
 
-  private boolean hasFireBoots;
-  private boolean hasFlippers;
-  private int tokens;
-  private int redKeys;
-  private int greenKeys;
-  private int blueKeys;
-  private int yellowKeys;
+	private boolean hasFireBoots;
+	private boolean hasFlippers;
+	private int tokens;
+	private int redKeys;
+	private int greenKeys;
+	private int blueKeys;
+	private int yellowKeys;
 
-  /**
-  * Creates a Player object
-  * @param location Takes a vector location when created
-  */
-  public Player (Vector location){
-    super (location);
-    this.hasFireBoots = false;
-    this.hasFlippers = false;
-    this.tokens = 0;
-    this.redKeys = 0;
-    this.greenKeys = 0;
-    this.blueKeys = 0;
-    this.yellowKeys = 0;
-  }
+	/**
+	 * Creates a Player object
+	 *
+	 * @param location Takes a vector location when created
+	 */
+	public Player(Vector location) {
+		super(location);
+		this.hasFireBoots = false;
+		this.hasFlippers = false;
+		this.tokens = 0;
+		this.redKeys = 0;
+		this.greenKeys = 0;
+		this.blueKeys = 0;
+		this.yellowKeys = 0;
+	}
 
-  /**
-  * Adds an item to the players inventory
-  * @param Item The item to be added
-  */
-  public void addItem (Item item){
-    ItemType add = item.getType;
-    switch(add) {
-      case TOKEN:
-        tokens++;
-        break;
-      case FIREBOOTS:
-        hasFireBoots = true;
-        break;
-      case FLIPPERS:
-        hasFlippers = true;
-        break;
-      case REDKEY:
-        redKeys++;
-        break;
-      case BLUEKEY:
-        blueKeys++;
-        break;
-      case GREENKEY:
-        greenKeys++;
-        break;
-      case YELLOWKEY:
-        yellowKeys++;
-        break;
-    }
-  }
+	/**
+	 * Creates a player object form a int array
+	 * {X,Y,fireShoes,flippers,tokens,red,green,blue,yellow}
+	 *
+	 * @param inventory
+	 */
+	public Player(int[] inventory) {
+		super(new Vector(inventory[0], inventory[1]));
+		if (inventory[2] == 1) {
+			this.hasFireBoots = true;
+		} else {
+			this.hasFireBoots = false;
+		}
+		if (inventory[3] == 1) {
+			this.hasFlippers = true;
+		} else {
+			this.hasFlippers = false;
+		}
+		this.tokens = inventory[4];
+		this.redKeys = inventory[5];
+		this.greenKeys = inventory[6];
+		this.blueKeys = inventory[7];
+		this.yellowKeys = inventory[8];
+	}
 
-  /**
-  * Method to get the player's location
-  * @return The player's vector
-  */
-  public Vector getPos (){
-    return pos;
-  }
+	/**
+	 * Adds an item to the players inventory
+	 *
+	 * @param Item The item to be added
+	 */
+	public void addItem(Item item) {
+		ItemType add = item.getType();
+		switch (add) {
+		case TOKEN:
+			tokens++;
+			break;
+		case FIREBOOTS:
+			hasFireBoots = true;
+			break;
+		case FLIPPERS:
+			hasFlippers = true;
+			break;
+		case REDKEY:
+			redKeys++;
+			break;
+		case BLUEKEY:
+			blueKeys++;
+			break;
+		case GREENKEY:
+			greenKeys++;
+			break;
+		case YELLOWKEY:
+			yellowKeys++;
+			break;
+		}
+	}
 
-  /**
-  * Method to return the number of tokens in the player's inventory
-  * @return The number of tokens
-  */
-  public int getTokens(){
-    return tokens;
-  }
+	/**
+	 * Method to get the player's location
+	 *
+	 * @return The player's vector
+	 */
+	public Vector getPos() {
+		return pos;
+	}
 
-  /**
-  * Method to find if the player has collected fire boots
-  * @return true if the player has picked up fire boots, else false
-  */
-  public Boolean hasFireBoots(){
-    return hasFireBoots;
-  }
+	/**
+	 * Method to change the player's location
+	 *
+	 * @param The player's new vector
+	 */
+	public void setPos(Vector newPos) {
+		this.pos = newPos;
+	}
 
-  /**
-  * Method to find if the player has collected flippers
-  * @return true if the player has picked up flippers, else false
-  */
-  public Boolean hasFlippers(){
-    return hasFlippers;
-  }
+	/**
+	 * Method to return the number of tokens in the player's inventory
+	 *
+	 * @return The number of tokens
+	 */
+	public int getTokens() {
+		return tokens;
+	}
 
-  /**
-  * Check for and use tokens from the player's inventory
-  * @param tokensReq Tokens required
-  * @return true if the tokens are present in the inventory, false otherwise
-  */
-  public Boolean useTokens(int tokensReq){
-    if (tokensReq <= tokens){
-      tokens = tokens - tokensReq;
-      return true;
-    } else {
-      return false;
-    }
-  }
+	/**
+	 * Method to find if the player has collected fire boots
+	 *
+	 * @return true if the player has picked up fire boots, else false
+	 */
+	public Boolean hasFireBoots() {
+		return hasFireBoots;
+	}
 
-  /**
-  * Method to check for and to use an item in the player's inventory
-  * @param item The item the game would like to use
-  * @return true if the item is present, false otherwise
-  */
-  public Boolean useItem(Item item){
-    ItemType use = item.getType;
-    switch(use) {
-      case FIREBOOTS:
-        if (hasFireBoots == true){
-          return true;
-        } else {
-          return false;
-        }
-      case FLIPPERS:
-        if (hasFlippers == true){
-          return true;
-        } else {
-          return false;
-        }
-      case REDKEY:
-        if (redKeys > 0){
-          redKeys--;
-          return true;
-        } else {
-          return false;
-        }
-      case BLUEKEY:
-        if (blueKeys > 0){
-          blueKeys--;
-          return true;
-        } else {
-          return false;
-        }
-      case GREENKEY:
-        if (greenKeys > 0){
-          greenKeys--;
-          return true;
-        } else {
-          return false;
-        }
-      case YELLOWKEY:
-        if (yellowKeys > 0){
-          yellowKeys--;
-          return true;
-        } else {
-          return false;
-        }
-    }
-  }
+	/**
+	 * Method to find if the player has collected flippers
+	 *
+	 * @return true if the player has picked up flippers, else false
+	 */
+	public Boolean hasFlippers() {
+		return hasFlippers;
+	}
+
+	/**
+	 * Check for and use tokens from the player's inventory
+	 *
+	 * @param tokensReq Tokens required
+	 * @return true if the tokens are present in the inventory, false otherwise
+	 */
+	public Boolean useTokens(int tokensReq) {
+		if (tokensReq <= tokens) {
+			tokens = tokens - tokensReq;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Method to check for and to use an item in the player's inventory
+	 *
+	 * @param item The item the game would like to use
+	 * @return true if the item is present, false otherwise
+	 */
+	public Boolean useItem(Item item) {
+		ItemType use = item.getType();
+		switch (use) {
+		case FIREBOOTS:
+			if (hasFireBoots == true) {
+				return true;
+			} else {
+				return false;
+			}
+		case FLIPPERS:
+			if (hasFlippers == true) {
+				return true;
+			} else {
+				return false;
+			}
+		case REDKEY:
+			if (redKeys > 0) {
+				redKeys--;
+				return true;
+			} else {
+				return false;
+			}
+		case BLUEKEY:
+			if (blueKeys > 0) {
+				blueKeys--;
+				return true;
+			} else {
+				return false;
+			}
+		case GREENKEY:
+			if (greenKeys > 0) {
+				greenKeys--;
+				return true;
+			} else {
+				return false;
+			}
+		case YELLOWKEY:
+			if (yellowKeys > 0) {
+				yellowKeys--;
+				return true;
+			} else {
+				return false;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Generate an array containing the player's inventory {X,Y,fireShoes, flippers,
+	 * tokens, red, green, blue, yellow}
+	 *
+	 * @return int array of player's inventory
+	 */
+	public int[] export() {
+		int fireShoes = 0;
+		int flippers = 0;
+		if (hasFireBoots() == true) {
+			fireShoes = 1;
+		}
+		if (hasFlippers() == true) {
+			flippers = 1;
+		}
+		int[] playerSave;
+		playerSave = new int[] { pos.getX(), pos.getY(), fireShoes, flippers, tokens, redKeys, greenKeys, blueKeys,
+				yellowKeys };
+		return playerSave;
+	}
 }
