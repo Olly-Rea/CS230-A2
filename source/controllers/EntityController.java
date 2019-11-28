8e8c52f30d1e434db4bf1fceb4d71a05c7d7560a
Scott <sbarr@SG-MC.local>
2019-11-28 14:53:37 +0000

    Added functionality to create an enemy when given the scanner of the details

--- source/controllers/EntityController.java
+++ source/controllers/EntityController.java
@@ -5,7 +5,15 @@ package controllers;
  * @version 1.1
  */
 import java.util.ArrayList;
+import java.util.Scanner;
+
 import entities.*;
+import entities.enemies.DumbTargeter;
+import entities.enemies.SmartTargeter;
+import entities.enemies.StraightLineEnemy;
+import entities.enemies.WallFollower;
+import utils.Direction;
+import utils.Rotation;
 import utils.Vector;
 
 public class EntityController {
@@ -21,12 +29,12 @@ public class EntityController {
      * @param enemies
      */
     public EntityController(Entity[][] entityGrid) {
-
+        this.entityGrid = entityGrid;
     }
 
     /**
-     * Checks if the player is standing on a item. Add the item if an item
-     * collision has occurred.
+     * Checks if the player is standing on a item. Add the item if an item collision
+     * has occurred.
      *
      * @param player The Player object for position reference.
      */
@@ -44,7 +52,7 @@ public class EntityController {
      * Checks whether a player has collided with an enemy.
      *
      * @param player The player object where the position will be checked in the
-     * entityGrid.
+     *               entityGrid.
      * @return True if the player has collided with an enemy. False otherwise.
      */
     public boolean enemyCollision(Player player) {
@@ -72,8 +80,8 @@ public class EntityController {
     /**
      * Iterates through each enemy and moves them to their next position.
      *
-     * @param map the map will be passed through to each enemy to assist their
-     * next move calculation.
+     * @param map the map will be passed through to each enemy to assist their next
+     *            move calculation.
      */
     public void moveEnemies(MapController map) {
         // private Vector enemyPos;
@@ -120,24 +128,129 @@ public class EntityController {
         return null;
     }
 
+    /**
+     * Static method to create a new Player object
+     * 
+     * @param line the scanner of a line with the player information
+     * @return the Player object
+     */
+    public static Player makePlayer(Scanner line) {
+        int x = line.nextInt();
+        int y = line.nextInt();
+        return new Player(x, y);
+    }
+
+    /**
+     * Creates a new straight line enemy from a scanner and position
+     * 
+     * @param line the scanner of the rest of the line including specific details
+     * @param pos  the position the enemy is created at
+     * @return the instance of the StraightLineEnemy
+     */
+    private static StraightLineEnemy makeSL(Scanner line, Vector pos) {
+        String faceDir = line.next();
+        switch (faceDir) {
+        case "UP":
+            return new StraightLineEnemy(pos, Direction.UP);
+        case "RIGHT":
+            return new StraightLineEnemy(pos, Direction.RIGHT);
+        case "DOWN":
+            return new StraightLineEnemy(pos, Direction.DOWN);
+        case "LEFT":
+            return new StraightLineEnemy(pos, Direction.LEFT);
+        default:
+            return new StraightLineEnemy(pos, Direction.UP);
+        }
+    }
+
+    /**
+     * Creates a new straight line enemy from a scanner and position
+     * 
+     * @param line the scanner of the rest of the line including specific details
+     * @param pos  the position the enemy is created at
+     * @return the instance of the StraightLineEnemy
+     */
+    private static WallFollower makeWF(Scanner line, Vector pos) {
+        Direction dir;
+        Rotation rot;
+
+        String faceDir = line.next();
+        String rotation = line.next();
+
+        switch (faceDir) {
+        case "UP":
+            dir = Direction.UP;
+        case "RIGHT":
+            dir = Direction.RIGHT;
+        case "DOWN":
+            dir = Direction.DOWN;
+        case "LEFT":
+            dir = Direction.LEFT;
+        default:
+            dir = Direction.UP;
+        }
+
+        switch (rotation) {
+        case "CW":
+            rot = Rotation.CW;
+        case "ACW":
+            rot = Rotation.ACW;
+        default:
+            rot = Rotation.ACW;
+        }
+
+        return new WallFollower(pos, dir, rot);
+    }
+
+    /**
+     * 
+     * @param line a scanner of the line with the enemy details
+     * @return
+     */
+    public static Enemy makeEnemy(Scanner line, Player p) {
+        int x = line.nextInt();
+        int y = line.nextInt();
+        String type = line.next();
+        switch (type) {
+        case "SL":
+            return makeSL(line, new Vector(x, y));
+        case "WF":
+            return makeWF(line, new Vector(x, y));
+        case "DT":
+            return new DumbTargeter(new Vector(x, y), p);
+        case "ST":
+            return new SmartTargeter(new Vector(x, y), p);
+        default:
+            return null;
+        }
+    }
+
+    /**
+     * Returns a new item based on the char in the map text file
+     * 
+     * @param x the x position of the item
+     * @param y the y positio of the item
+     * @param c the character in the map file
+     * @return an instance of an item depending on the character given
+     */
     public static Entity makeEntity(int x, int y, char c) {
         switch (c) {
-            case 'r':
-                return new Item(ItemType.REDKEY, x, y);
-            case 'g':
-                return new Item(ItemType.GREENKEY, x, y);
-            case 'b':
-                return new Item(ItemType.BLUEKEY, x, y);
-            case 'y':
-                return new Item(ItemType.YELLOWKEY, x, y);
-            case 'f':
-                return new Item(ItemType.FIREBOOTS, x, y);
-            case 'w':
-                return new Item(ItemType.FLIPPERS, x, y);
-            case '*':
-                return new Item(ItemType.TOKEN, x, y);
-            default:
-                return null;
+        case 'r':
+            return new Item(ItemType.REDKEY, x, y);
+        case 'g':
+            return new Item(ItemType.GREENKEY, x, y);
+        case 'b':
+            return new Item(ItemType.BLUEKEY, x, y);
+        case 'y':
+            return new Item(ItemType.YELLOWKEY, x, y);
+        case 'f':
+            return new Item(ItemType.FIREBOOTS, x, y);
+        case 'w':
+            return new Item(ItemType.FLIPPERS, x, y);
+        case '*':
+            return new Item(ItemType.TOKEN, x, y);
+        default:
+            return null;
         }
     }
 
