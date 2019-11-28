66906a9d4045bed2b907e42473078e8898b3af4c
Scott <sbarr@SG-MC.local>
2019-11-28 15:12:08 +0000

    commented invalid code and added getPlayer method

--- source/controllers/PlayerController.java
+++ source/controllers/PlayerController.java
@@ -38,21 +38,21 @@ public class PlayerController {
      */
     public void move(MOVES dir, MapController mc) {
         Vector current = getPlayerPos();
-        Vector desired;
-        if (dir == MOVES.UP) {
-            desired = new Vector(current.getX(), current.getY() + 1);
-        } else if (dir == MOVES.DOWN) {
-            desired = new Vector(current.getX(), current.getY() - 1);
-        } else if (dir == MOVES.LEFT) {
-            desired = new Vector(current.getX() - 1, current.getY());
-        } else if (dir == MOVES.RIGHT) {
-            desired = new Vector(current.getX() + 1, current.getY());
-        }
-        Cell desiredCell = mc.getCell(desired.getX(), desired.getY());
-        if (validMove(desiredCell)) {
-            player.setPos(desired);
-            // mc.render? Set new location to render around
-        }
+        // Vector desired;
+        // if (dir == MOVES.UP) {
+        //     desired = new Vector(current.getX(), current.getY() + 1);
+        // } else if (dir == MOVES.DOWN) {
+        //     desired = new Vector(current.getX(), current.getY() - 1);
+        // } else if (dir == MOVES.LEFT) {
+        //     desired = new Vector(current.getX() - 1, current.getY());
+        // } else if (dir == MOVES.RIGHT) {
+        //     desired = new Vector(current.getX() + 1, current.getY());
+        // }
+        // Cell desiredCell = mc.getCell(desired.getX(), desired.getY());
+        // if (validMove(desiredCell)) {
+        //     player.setPos(desired);
+        //     // mc.render? Set new location to render around
+        // }
     }
 
     /**
@@ -66,26 +66,26 @@ public class PlayerController {
     private boolean validMove(Cell targetCell) {
         CellType move = targetCell.getType();
         Boolean valid = null;
-        if (move == (CellType.GROUND) || move == (CellType.FIRE) || move == (CellType.WATER)) {
-            valid = true;
-        } else if (move == (CellType.RED)) {
-            Item redKey = new Item(ItemType.REDKEY);
-            valid = player.useItem(redKey);
-        } else if (move == (CellType.BLUE)) {
-            Item blueKey = new Item(ItemType.BLUEKEY);
-            valid = player.useItem(blueKey);
-        } else if (move == (CellType.YELLOW)) {
-            Item yellowKey = new Item(ItemType.YELLOWKEY);
-            valid = player.useItem(yellowKey);
-        } else if (move == (CellType.GREEN)) {
-            Item greenKey = new Item(ItemType.GREENKEY);
-            valid = player.useItem(greenKey);
-        } else if (move == (CellType.TOKEN)) {
-            // valid = player.useTokens(); // Need method to check tokens required
-        } else if (move == (CellType.TELEPORTER)) {
-            valid = true;
-            // player.setPos(Cell.getLinkedPos());
-        }
+        // if (move == (CellType.GROUND) || move == (CellType.FIRE) || move == (CellType.WATER)) {
+        //     valid = true;
+        // } else if (move == (CellType.RED)) {
+        //     Item redKey = new Item(ItemType.REDKEY);
+        //     valid = player.useItem(redKey);
+        // } else if (move == (CellType.BLUE)) {
+        //     Item blueKey = new Item(ItemType.BLUEKEY);
+        //     valid = player.useItem(blueKey);
+        // } else if (move == (CellType.YELLOW)) {
+        //     Item yellowKey = new Item(ItemType.YELLOWKEY);
+        //     valid = player.useItem(yellowKey);
+        // } else if (move == (CellType.GREEN)) {
+        //     Item greenKey = new Item(ItemType.GREENKEY);
+        //     valid = player.useItem(greenKey);
+        // } else if (move == (CellType.TOKEN)) {
+        //     // valid = player.useTokens(); // Need method to check tokens required
+        // } else if (move == (CellType.TELEPORTER)) {
+        //     valid = true;
+        //     // player.setPos(Cell.getLinkedPos());
+        // }
         return valid;
     }
 
@@ -118,6 +118,15 @@ public class PlayerController {
      */
     public Vector getPlayerPos() {
         return player.getPos();
+    }
+
+    /**
+     * Returns the object of the player
+     * 
+     * @return the player object
+     */
+    public Player getPlayer() {
+        return player;
     }
 
     /**
