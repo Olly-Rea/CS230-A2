66b5687d4630c2cb2a806bfbda4eb2542aaa266e
Scott <sbarr@SG-MC.local>
2019-11-28 12:32:17 +0000

    quickfix for Entity

--- source/entities/Entity.java
+++ source/entities/Entity.java
@@ -18,13 +18,20 @@ public abstract class Entity {
     /**
      * Constructs an entity at a position taken from the Map
      *
-     * @param pos The position at which the entity will be created
+     * @param x The x position value for the entity
+     * @param y The y position value for the entity
      */
     public Entity(int x, int y) {
         this.pos = new Vector(x, y);
     }
+
+    /**
+     * Constructs an enttiy from a vector
+     * 
+     * @param pos Position vector of the entity
+     */
     public Entity(Vector pos) {
-        this.pos = pos);
+        this.pos = pos;
     }
 
     /**
