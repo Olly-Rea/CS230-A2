e911363178959756c6c3e98c2c07567cd61a7eb1
Olly Rea <Oliver.m.rea@gmail.com>
2019-11-29 16:16:53 +0000

    Slight changes to GameController and Extra Features file

--- source/controllers/GameController.java
+++ source/controllers/GameController.java
@@ -5,205 +5,207 @@ import javafx.scene.Group;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.transform.Scale;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 //Local imports
 import java.util.Scanner;
 import cells.Cell;
 import entities.Entity;
 import entities.Item;
+import misc.Profile;
+import utils.*;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import misc.Profile;
-import utils.*;
 
 /**
  *
  */
 public class GameController {
 
-	private static final String PROFILE_PATH = "...";
-	private static final String MAP_DIR = "...";
-	private static final String SAVE_DIR = "...";
-	private static final String LEADERBOARD_DIR = "...";
+    private static final String PROFILE_PATH = "...";
+    private static final String MAP_DIR = "...";
+    private static final String SAVE_DIR = "...";
+    private static final String LEADERBOARD_DIR = "...";
 
-	private MapController mapController;
-	private PlayerController playerController;
-	private EntityController entityController;
-	private Profile currentProfile;
-	private int startTime;
-	private String currentMap;
+    private MapController mapController;
+    private PlayerController playerController;
+    private EntityController entityController;
+    private Profile currentProfile;
+    private int startTime;
+    private String currentMap;
+    
+    //X and Y variables for render translate methods
+    private double renderX = 0;
+    private double renderY = 0;
 
-	/**
-	 *
-	 */
-	public GameController() {
-		loadGame("./levelfiles/Test_File_DD.txt");
-	}
+    /**
+     *
+     */
+    public GameController() {
+        loadGame("./levelfiles/Test_File_DD.txt");
+    }
 
-	/**
-	 * Creates a 2d Entity Map and Cell Map and stores them in the mapController and
-	 * entityController
-	 *
-	 * @param fh The Handler reading the Map/LoadFile
-	 * @return A 2d array of cells to construct the MapController with
-	 */
-	private void makeControllers(FileHandler fh) {
-		String init = fh.nextLine();
-		Scanner sc = new Scanner(init);
-		int mapWidth = sc.nextInt();
-		int mapHeight = sc.nextInt();
+    /**
+     * Creates a 2d Entity Map and Cell Map and stores them in the mapController
+     * and entityController
+     *
+     * @param fh The Handler reading the Map/LoadFile
+     * @return A 2d array of cells to construct the MapController with
+     */
+    private void makeControllers(FileHandler fh) {
+        String init = fh.nextLine();
+        Scanner sc = new Scanner(init);
+        int mapWidth = sc.nextInt();
+        int mapHeight = sc.nextInt();
 
-		Cell[][] map = new Cell[mapHeight][mapWidth];
-		Entity[][] entityMap = new Entity[mapHeight][mapWidth];
-		entityController = new EntityController(entityMap);
+        Cell[][] map = new Cell[mapHeight][mapWidth];
+        Entity[][] entityMap = new Entity[mapHeight][mapWidth];
+        entityController = new EntityController(entityMap);
 
-		for (int y = 0; y < mapHeight; y++) {
-			String row = fh.nextLine();
-			for (int x = 0; x < mapWidth; x++) {
-				char c = row.charAt(x);
-				map[y][x] = MapController.makeCell(x, y, c);
-				Item e = EntityController.makeItem(x, y, c);
-				entityController.addItem(e);
-			}
-		}
-		sc.close();
+        for (int y = 0; y < mapHeight; y++) {
+            String row = fh.nextLine();
+            for (int x = 0; x < mapWidth; x++) {
+                char c = row.charAt(x);
+                map[y][x] = MapController.makeCell(x, y, c);
+                Item e = EntityController.makeItem(x, y, c);
+                entityController.addItem(e);
+            }
+        }
+        sc.close();
 
-		mapController = new MapController(map, mapWidth, mapHeight);
-	}
+        mapController = new MapController(map, mapWidth, mapHeight);
+    }
 
-	private void handleSpecific(String line) {
-		Scanner sc = new Scanner(line);
-		sc.useDelimiter(" ");
-		String keyword = sc.next();
+    private void handleSpecific(String line) {
+        Scanner sc = new Scanner(line);
+        sc.useDelimiter(" ");
+        String keyword = sc.next();
 
-		switch (keyword) {
-		case "PLAYER":
-			playerController = new PlayerController(EntityController.makePlayer(sc));
-			break;
-		case "ENEMY":
-			entityController.addEnemy(EntityController.makeEnemy(sc, playerController.getPlayer()));
-			break;
-		case "TELEPORTER":
-			mapController.linkTeleporters(sc);
-			break;
-		case "DOOR":
-			mapController.initDoor(sc);
-			break;
-		}
+        switch (keyword) {
+            case "PLAYER":
+                playerController = new PlayerController(EntityController.makePlayer(sc));
+                break;
+            case "ENEMY":
+                entityController.addEnemy(EntityController.makeEnemy(sc, playerController.getPlayer()));
+                break;
+            case "TELEPORTER":
+                mapController.linkTeleporters(sc);
+                break;
+            case "DOOR":
+                mapController.initDoor(sc);
+                break;
+        }
 
-		sc.close();
-	}
+        sc.close();
+    }
 
-	/**
-	 * Loads a path to a map file and to generate objects for use in the game.
-	 *
-	 * @param path Path to the map file.
-	 */
-	public void loadGame(String path) {
-		FileHandler fh = new FileHandler(path);
-		makeControllers(fh);
+    /**
+     * Loads a path to a map file and to generate objects for use in the game.
+     *
+     * @param path Path to the map file.
+     */
+    public void loadGame(String path) {
+        FileHandler fh = new FileHandler(path);
+        makeControllers(fh);
 
-		// Specific Details
-		while (fh.hasNext()) {
-			handleSpecific(fh.nextLine());
-		}
-	}
+        // Specific Details
+        while (fh.hasNext()) {
+            handleSpecific(fh.nextLine());
+        }
+    }
 
-	/**
-	 *
-	 * @param path
-	 */
-	public void saveGame(String path) {
-		// Get MapController Export
-		// Get PlayerController Export
-		// Get EntityController Export
-	}
+    /**
+     *
+     * @param path
+     */
+    public void saveGame(String path) {
+        // Get MapController Export
+        // Get PlayerController Export
+        // Get EntityController Export
+    }
 
-	/**
-	 * Returns a list of profiles from the file at {@code PROFILE_PATH}.
-	 *
-	 * @return array of profiles retrieved from {@code PROFILE_PATH}.
-	 */
-	public Profile[] loadProfiles() {
-		return null;
-	}
+    /**
+     * Returns a list of profiles from the file at {@code PROFILE_PATH}.
+     *
+     * @return array of profiles retrieved from {@code PROFILE_PATH}.
+     */
+    public Profile[] loadProfiles() {
+        return null;
+    }
 
-	/**
-	 * Adds a profile to the file at {@code PROFILE_PATH} of the name {@code name}.
-	 *
-	 * @param name name to be added to the profile list.
-	 */
-	public void addProfile(String name) {
+    /**
+     * Adds a profile to the file at {@code PROFILE_PATH} of the name
+     * {@code name}.
+     *
+     * @param name name to be added to the profile list.
+     */
+    public void addProfile(String name) {
 
-	}
+    }
 
-	/**
-	 * Deletes the specific profile from the file at {@code PROFILE_PATH}.
-	 *
-	 * @param profile The profile to be deleted.
-	 */
-	public void deleteProfile(Profile profile) {
+    /**
+     * Deletes the specific profile from the file at {@code PROFILE_PATH}.
+     *
+     * @param profile The profile to be deleted.
+     */
+    public void deleteProfile(Profile profile) {
 
-	}
+    }
 
-	/**
-	 * Progresses the game 1 step and handles the key pressed.
-	 *
-	 * @param ke Key Event that was pressed by the user.
-	 */
-	public void gameStep(KeyEvent ke) {
+    /**
+     * Progresses the game 1 step and handles the key pressed.
+     *
+     * @param ke Key Event that was pressed by the user.
+     */
+    public void gameStep(KeyEvent ke) {
 
-		switch (ke.getCode()) {
-		case RIGHT:
-			playerController.move(Direction.RIGHT, mapController);
-			break;
-		case LEFT:
-			playerController.move(Direction.LEFT, mapController);
-			break;
-		case UP:
-			playerController.move(Direction.UP, mapController);
-			break;
-		case DOWN:
-			playerController.move(Direction.DOWN, mapController);
-			break;
-		case ESCAPE:
-			// Bring up menu?
-			break;
-		default:
-			// Do nothing
-			break;
+        switch (ke.getCode()) {
+            case RIGHT:
+                playerController.move(Direction.RIGHT, mapController);
+                break;
+            case LEFT:
+                playerController.move(Direction.LEFT, mapController);
+                break;
+            case UP:
+                playerController.move(Direction.UP, mapController);
+                break;
+            case DOWN:
+                playerController.move(Direction.DOWN, mapController);
+                break;
+            case ESCAPE:
+                // Bring up menu?
+                break;
+            default:
+                // Do nothing
+                break;
 
-		// Check if player is dead
+            // Check if player is dead
+            // if (playerController.checkStatus(mapController) ||
+            // entityController.enemyCollision(playerController.getPlayer())){
+            // // Restart game
+            // }
+            // event.consume;
+        }
+    }
 
-		// if (playerController.checkStatus(mapController) ||
-		// entityController.enemyCollision(playerController.getPlayer())){
-		// // Restart game
-		// }
+    /**
+     * Shows a leaderboard for a specific map in {@code LEADERBOARD_DIR}.
+     *
+     * @param path The file path inside {@code LEADERBOARD_DIR} for the map.
+     */
+    public void showLeaderboard(String path) {
 
-		// event.consume;
+    }
 
-		}
-	}
+    /**
+     * adds a time to the map time file in {@code LEADERBOARD_DIR}.
+     *
+     * @param path THe file path for the map.
+     */
+    public void addMapTime(String path) {
 
-	/**
-	 * Shows a leaderboard for a specific map in {@code LEADERBOARD_DIR}.
-	 *
-	 * @param path The file path inside {@code LEADERBOARD_DIR} for the map.
-	 */
-	public void showLeaderboard(String path) {
-
-	}
-
-	/**
-	 * adds a time to the map time file in {@code LEADERBOARD_DIR}.
-	 *
-	 * @param path THe file path for the map.
-	 */
-	public void addMapTime(String path) {
-
-	}
+    }
 
     public void render(Group root, double scaleVal) {
 
@@ -235,11 +237,7 @@ public class GameController {
         featherEdge.getTransforms().add(new Scale(scaleVal, scaleVal, 0, 0));
         root.getChildren().add(featherEdge);
 
-
     }
-
-    private double renderX = 0;
-    private double renderY = 0;
 
     public void renderMove(Group root, int x, int y, double scaleVal) {
 
