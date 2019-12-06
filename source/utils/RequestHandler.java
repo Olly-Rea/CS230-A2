beaca18cebca8c83bdc52bf8cbf8cb5b1ee24726
James Hogg <44364237+JamesH0gg@users.noreply.github.com>
2019-12-06 16:43:11 +0000

    updated javadoc

--- source/utils/RequestHandler.java
+++ source/utils/RequestHandler.java
@@ -26,7 +26,7 @@ public class RequestHandler {
     /**
      * Sends a get request to the url with specific query parameters.
      *
-     * @param url
+     * @param uri used to create a new url
      * @param params
      */
     public static String get(String uri) {
@@ -46,7 +46,10 @@ public class RequestHandler {
 
         return null;
     }
-
+    /**
+    * Deciphers the puzzle taken from the Url
+    * @param puzzle the puzzle to be deciphered
+    */
     public static String decipher(String puzzle) {
         char[] chars = puzzle.toCharArray();
         int val = 1;
