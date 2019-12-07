7cfa11bdf1ff8a18fd2ebe82af49ebd9f79145b4
treik2 <48773729+treik2@users.noreply.github.com>
2019-12-07 14:28:54 +0000

    Updated sounds

--- source/utils/SoundHandler.java
+++ source/utils/SoundHandler.java
@@ -1,32 +1,47 @@
 package utils;
 
-import javafx.scene.media;
+import java.io.File;
 
-public class SoundHandler {
+import javafx.application.Application;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
+
+public class SoundHandler extends Application {
+
+    private MediaPlayer mediaPlayer;
 
     //Constructor of the class
     public SoundHandler (String path) {
-        Media media = new Media("C:/GitHub/CS230-A2/assets/sounds/Cave background ambience/Cave.mp3");
+        File f = new File(path);
+        Media media = new Media(f.toURI().toString());
         mediaPlayer = new MediaPlayer(media);
     }
 
     //A static method to play the audio that is selected.
-    public static void play () {
+    public void play () {
         mediaPlayer.setAutoPlay(true);
     }
 
     //A static method to stop the audio that is playing
-    public static void stop() {
-        mediaPlayer.setOnStopped();
+    public void stop() {
+        mediaPlayer.pause();
+        // mediaPlayer.stop();
     }
     
     //A static method to mute the current song/ambience that is playing
-    public static void mute() {
-        mediaPlayer.SetMute(true);
+    public void mute() {
+        mediaPlayer.setMute(true);
     }
 
-    
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-      
+    @Override
+    public void start(Stage stage) {
+        SoundHandler sh = new SoundHandler("./assets/sounds/Menu/songMenu1.mp3");
+        sh.play();
+    }
 
 }
