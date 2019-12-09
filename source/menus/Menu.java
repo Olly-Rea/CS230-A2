81097da62c293d288602c0da7528651ae153b7c2
Olly Rea <Oliver.m.rea@gmail.com>
2019-12-09 00:20:06 +0000

    Added menu javadoc

--- source/menus/Menu.java
+++ source/menus/Menu.java
@@ -17,15 +17,26 @@ public abstract class Menu {
     double scaleVal = GameController.SCALE_VAL;
     protected VBox menuLayout = new VBox(50);
 
+    /**
+     * Constructor for the menu superclass
+     */
     public Menu() {
         menuLayout.setVisible(false);
         menuLayout.getStylesheets().add("File:./assets/styles/menu.css");
     }
 
+    /**
+     * Render method for each menu, allowing it to be displayed to the screen
+     * 
+     * @return the VBox containing the menu child nodes
+     */
     public VBox render() {
         return menuLayout;
     }
 
+    /**
+     * Method to toggle the visibility of a menu - Fade transition for cool points
+     */
     public void toggle() {
         boolean in = menuLayout.isVisible() == false;
         menuLayout.setVisible(true);
@@ -41,10 +52,18 @@ public abstract class Menu {
         });
     }
 
+    /**
+     * Method to get a boolean if a menu is visible or not
+     * 
+     * @return the boolean VBox isVisible() value
+     */
     public boolean isVisible() {
         return menuLayout.isVisible();
     }
     
+    /**
+     * Method to scale the menus to fit in the scene window
+     */
     public void scaleMenu() {
         double menuWidth = 1380 - menuLayout.getWidth();
         double menuHeight = 1380 - menuLayout.getHeight();
